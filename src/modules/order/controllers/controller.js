2b1d5f4dbee3e94a37bec6f2eb0ba45e1b11e080
jigkoh3 <jigkoh3@hotmail.com>
2019-08-19 15:13:58 +0700

    fix issue other product items

--- src/modules/order/controllers/controller.js
+++ src/modules/order/controllers/controller.js
@@ -88,82 +88,93 @@ exports.mapData = function (req, res, next) {
                 _order.items.push({
                     "name": "ลิปมินิ",
                     "price": 0,
-                    "totalqty": 0,
+                    "totalqty": (order.mini_01 || 0)
+                        + (order.mini_02 || 0)
+                        + (order.mini_03 || 0)
+                        + (order.mini_04 || 0)
+                        + (order.mini_05 || 0)
+                        + (order.mini_06 || 0)
+                        + (order.mini_07 || 0)
+                        + (order.mini_08 || 0)
+                        + (order.mini_09 || 0)
+                        + (order.mini_10 || 0)
+                        + (order.mini_11 || 0)
+                        + (order.mini_12 || 0),
                     "amount": 0,
-                    "option": [{ "name": "ประเภท", "value": [] }]
+                    "option": [{ "name": "เบอร์", "value": [] }]
                 });
 
                 if (order.mini_01) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "1",
                         qty: order.mini_01
                     });
                 }
 
                 if (order.mini_02) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "2",
                         qty: order.mini_02
                     });
                 }
 
                 if (order.mini_03) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "3",
                         qty: order.mini_03
                     });
                 }
 
                 if (order.mini_04) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "4",
                         qty: order.mini_04
                     });
                 }
                 if (order.mini_05) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "5",
                         qty: order.mini_05
                     });
                 }
                 if (order.mini_06) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "6",
                         qty: order.mini_06
                     });
                 }
                 if (order.mini_07) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "7",
                         qty: order.mini_07
                     });
                 }
                 if (order.mini_08) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "8",
                         qty: order.mini_08
                     });
                 }
                 if (order.mini_09) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "9",
                         qty: order.mini_09
                     });
                 }
                 if (order.mini_10) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "10",
                         qty: order.mini_10
                     });
                 }
                 if (order.mini_11) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "11",
                         qty: order.mini_11
                     });
                 }
                 if (order.mini_12) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "12",
                         qty: order.mini_12
                     });
@@ -185,81 +196,93 @@ exports.mapData = function (req, res, next) {
                 _order.items.push({
                     "name": "ลิป 5 กรัม",
                     "price": 0,
-                    "totalqty": 0,
-                    "amount": 0
+                    "totalqty": (order.large_01 || 0)
+                        + (order.large_02 || 0)
+                        + (order.large_03 || 0)
+                        + (order.large_04 || 0)
+                        + (order.large_05 || 0)
+                        + (order.large_06 || 0)
+                        + (order.large_07 || 0)
+                        + (order.large_08 || 0)
+                        + (order.large_09 || 0)
+                        + (order.large_10 || 0)
+                        + (order.large_11 || 0)
+                        + (order.large_12 || 0),
+                    "amount": 0,
+                    "option": [{ "name": "เบอร์", "value": [] }]
                 });
 
                 if (order.large_01) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "1",
                         qty: order.large_01
                     });
                 }
 
                 if (order.large_02) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "2",
                         qty: order.large_02
                     });
                 }
 
                 if (order.large_03) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "3",
                         qty: order.large_03
                     });
                 }
 
                 if (order.large_04) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "4",
                         qty: order.large_04
                     });
                 }
                 if (order.large_05) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "5",
                         qty: order.large_05
                     });
                 }
                 if (order.large_06) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "6",
                         qty: order.large_06
                     });
                 }
                 if (order.large_07) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "7",
                         qty: order.large_07
                     });
                 }
                 if (order.large_08) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "8",
                         qty: order.large_08
                     });
                 }
                 if (order.large_09) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "9",
                         qty: order.large_09
                     });
                 }
                 if (order.large_10) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "10",
                         qty: order.large_10
                     });
                 }
                 if (order.large_11) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "11",
                         qty: order.large_11
                     });
                 }
                 if (order.large_12) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "12",
                         qty: order.large_12
                     });
@@ -272,26 +295,29 @@ exports.mapData = function (req, res, next) {
                 _order.items.push({
                     "name": "แป้ง cc&long lasting",
                     "price": 0,
-                    "totalqty": 0,
-                    "amount": 0
+                    "totalqty": (order.pub_01 || 0)
+                        + (order.pub_02 || 0)
+                        + (order.pub_03 || 0),
+                    "amount": 0,
+                    "option": [{ "name": "เบอร์", "value": [] }]
                 });
 
                 if (order.pub_01) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "1",
                         qty: order.pub_01
                     });
                 }
 
                 if (order.pub_02) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "2",
                         qty: order.pub_02
                     });
                 }
 
                 if (order.pub_03) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "3",
                         qty: order.pub_03
                     });
@@ -303,26 +329,29 @@ exports.mapData = function (req, res, next) {
                 _order.items.push({
                     "name": "แป้ง all in one perfect",
                     "price": 0,
-                    "totalqty": 0,
-                    "amount": 0
+                    "totalqty": (order.pb_01 || 0)
+                        + (order.pb_02 || 0)
+                        + (order.pb_03 || 0),
+                    "amount": 0,
+                    "option": [{ "name": "เบอร์", "value": [] }]
                 });
 
                 if (order.pb_01) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "1",
                         qty: order.pb_01
                     });
                 }
 
                 if (order.pb_02) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "2",
                         qty: order.pb_02
                     });
                 }
 
                 if (order.pb_03) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "3",
                         qty: order.pb_03
                     });
@@ -333,32 +362,34 @@ exports.mapData = function (req, res, next) {
                 _order.items.push({
                     "name": "สบู่",
                     "price": 0,
-                    "totalqty": 0,
-                    "amount": 0
+                    "totalqty": (order.soappink || 0)
+                        + (order.soaporange || 0),
+                    "amount": 0,
+                    "option": [{ "name": "สี", "value": [] }]
                 });
 
                 if (order.soappink) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "pink",
                         qty: order.soappink
                     });
                 }
 
                 if (order.soaporange) {
-                    _order.items[0].option[0].value.push({
+                    _order.items[_order.items.length -1].option[0].value.push({
                         name: "orange",
                         qty: order.soaporange
                     });
                 }
             }
 
-            if(order.reward){
-                // _order.items.push({
-                //     "name": "**",
-                //     "price": 0,
-                //     "totalqty": 0,
-                //     "amount": 0
-                // });
+            if (order.reward) {
+                _order.rewards.push({
+                    "name": order.reward,
+                    "price": 0,
+                    "totalqty": 1,
+                    "amount": 0
+                });
             }
 
             if (order.codamount) {
